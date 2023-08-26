e40775ef61d27e29ddbfd76657f5c16c1fa7c5dd
Jorge Rosas <rosasjorge1701@gmail.com>
2023-08-26 17:29:58 -0300

    Added Rectangle struct that implementes Boundary

--- collision/collision.go
+++ collision/collision.go
@@ -1,5 +1,12 @@
 package collision
 
+type Rectangle struct {
+	X      float64
+	Y      float64
+	Width  float64
+	Height float64
+}
+
 type Boundary interface {
 	GetX() float64
 	GetY() float64
@@ -12,4 +19,20 @@ func CheckCollision(b1 Boundary, b2 Boundary) bool {
 		b1.GetX()+b1.GetWidth() > b2.GetX() &&
 		b1.GetY() < b2.GetY()+b2.GetHeight() &&
 		b1.GetY()+b1.GetHeight() > b2.GetY()
+}
+
+func (r *Rectangle) GetX() float64 {
+	return r.X
+}
+
+func (r *Rectangle) GetY() float64 {
+	return r.Y
+}
+
+func (r *Rectangle) GetWidth() float64 {
+	return r.Width
+}
+
+func (r *Rectangle) GetHeight() float64 {
+	return r.Height
 }
