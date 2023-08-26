fd782afcf32c6ef7f101d92ffdee84d5ae8a60f8
Jorge Rosas <rosasjorge1701@gmail.com>
2023-08-26 17:10:14 -0300

    Add bullet logic

---
+++ game/bullet.go
@@ -0,0 +1,54 @@
+package game
+
+import (
+	"game/render"
+	"math"
+)
+
+type Bullet struct {
+	x float64
+	y float64
+
+	xa float64
+	ya float64
+
+	texture *render.Texture
+
+	game *Game
+}
+
+func NewBullet(game *Game, x, y, dir float64) *Bullet {
+	return &Bullet{
+		x:       x,
+		y:       y,
+		xa:      math.Cos(dir),
+		ya:      math.Sin(dir),
+		texture: render.AllTextures["bullet"],
+		game:    game,
+	}
+}
+
+func (b *Bullet) Update() {
+	b.x += b.xa
+	b.y += b.ya
+}
+
+func (b *Bullet) GetX() float64 {
+	return b.x
+}
+
+func (b *Bullet) GetY() float64 {
+	return b.y
+}
+
+func (b *Bullet) GetTexture() *render.Texture {
+	return b.texture
+}
+
+func (b *Bullet) GetWidth() float64 {
+	return b.texture.Width
+}
+
+func (b *Bullet) GetHeight() float64 {
+	return b.texture.Height
+}
