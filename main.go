84850752e47fde8c90ddfeef4d0ca381eebee793
Jorge Rosas <rosasjorge1701@gmail.com>
2023-08-26 21:21:52 -0300

    Added image loading

--- main.go
+++ main.go
@@ -2,111 +2,47 @@ package main
 
 import (
 	"fmt"
-	"game/audio"
 	"game/configuration"
-	"game/game"
-	"game/render"
+	"game/images"
 	"os"
-	"runtime"
 
-	"github.com/veandco/go-sdl2/sdl"
-	"github.com/veandco/go-sdl2/ttf"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-func run() int {
-	runtime.LockOSThread()
+var img *ebiten.Image
 
-	if err := ttf.Init(); err != nil {
-		fmt.Println("ERROR Initializing TTF")
-		return 500
-	}
-	defer ttf.Quit()
+type Game struct{}
 
-	// Initialize audio
-	if err := audio.Initialize(); err != nil {
-		fmt.Println("ERROR Initializing audio:", err)
-		return 600
-	}
-	defer audio.Clear()
+func (g *Game) Update() error {
+	return nil
+}
 
-	// Creating window
-	window, err := sdl.CreateWindow(
-		configuration.Title,
-		sdl.WINDOWPOS_UNDEFINED,
-		sdl.WINDOWPOS_UNDEFINED,
-		configuration.Width,
-		configuration.Height,
-		sdl.WINDOW_SHOWN,
-	)
-	if err != nil {
-		fmt.Println("ERROR CreateWindow:", err)
-		return 100
-	}
-	defer window.Destroy()
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
 
-	// Creating renderer
-	r, err := render.NewRenderer(window)
-	if err != nil {
-		fmt.Println("ERROR CreateRenderer:", err)
-		return 200
-	}
-	defer r.Destroy()
+	images.AllImages["background"].Draw(screen, 0, 0)
+	images.AllImages["player"].Draw(screen, 10, 10)
+}
 
-	// Load texts
-	gameOverTexture, err := render.NewTextTexture(r, "Game Over!", 200, 50)
-	if err != nil {
-		fmt.Println("ERROR Creating text texture:", err)
-		return 501
-	}
-	defer gameOverTexture.Destroy()
-
-	// Load textures
-	render.LoadAllTextures(r)
-	defer render.ClearAllTextures()
-
-	// Loading background texture
-	background, err := render.NewTexture(r, "resources/background.bmp", 400, 350)
-	if err != nil {
-		fmt.Println("ERROR Loading background:", err)
-		return 300
-	}
-	defer background.Destroy()
-
-	// Creating game object
-	g, err := game.NewGame(r)
-	if err != nil {
-		fmt.Println("ERROR Creating new game:", err)
-		return 400
-	}
-
-	// Game loop
-	running := true
-	for running {
-		// Check for quit input
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
-			case sdl.QuitEvent:
-				running = false
-			}
-		}
-
-		r.RenderTexture(background, 0, 0)
-
-		if g.IsGameOver {
-			r.RenderTexture(gameOverTexture, 0, 0)
-		} else {
-			g.Update()
-		}
-
-		g.Render()
-
-		r.Present()
-		sdl.Delay(16)
-	}
-
-	return 0
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return configuration.Width, configuration.Height
 }
 
 func main() {
-	os.Exit(run())
+	// Load all images
+	if err := images.LoadImages(); err != nil {
+		fmt.Println("Error loading images:", err)
+		os.Exit(1)
+	}
+
+	// Create window
+	ebiten.SetWindowTitle(configuration.Title)
+	ebiten.SetWindowSize(configuration.Width, configuration.Height)
+
+	// Run game loop
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		fmt.Println("Error running game loop:", err)
+		os.Exit(1)
+	}
 }
